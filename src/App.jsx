e1a7cec9b4355bf8d4854fbb8711c81eb9c5fb04
Darya <daryapogas@gmail.com>
2025-03-30 23:16:23 -0500

    render each static todo in a TodoListItem component contain a paragraph containing a value controlled by state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
+import TodoListItem from './TodoListItem'
 
 function App() {
+  const [newTodo, setNewTodo] = useState('New tasks:')
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm/>
+      <p>{newTodo}</p>
       <TodoList/>
-      
     </div>
   )
 }
