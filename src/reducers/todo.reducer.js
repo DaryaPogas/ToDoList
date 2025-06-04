582972b3415ab040ac1eeb021a70fbaa2afed96a
Darya <daryapogas@gmail.com>
2025-06-04 00:40:23 -0500

    duplicate state logic to reducer

--- src/reducers/todo.reducer.js
+++ src/reducers/todo.reducer.js
@@ -15,58 +15,126 @@ const actions = {
   revertTodo: 'revertTodo',
   //action on Dismiss Error button
   clearError: 'clearError',
-}
+};
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+
     case actions.fetchTodos:
       return {
         ...state,
+        isLoading: true
       };
+
     case actions.loadTodos:
       return {
         ...state,
+        todoList: action.records.map((record) => ({
+          id: record.id,
+            ...record.fields,
+            createdTime: record.createdTime,
+            isCompleted: record.fields.isCompleted || false
+        })),
+        isLoading: false
       };
+
     case actions.setLoadError:
       return {
         ...state,
+        errorMessage: action.error.message,
+        isLoading: false
       };
+
     case actions.startRequest:
       return {
         ...state,
+        isSaving: true,
       };
+
     case actions.addTodo:
       return {
         ...state,
-      };
+        todoList: [
+          {
+            id: action.record.id,
+            ...action.record.fields,
+            createdTime: action.record.createdTime,
+            isCompleted: action.record.fields.isCompleted || false,
+          },
+          ...state.todoList],
+        isSaving: false
+      }
+
     case actions.endRequest:
       return {
         ...state,
+        isLoading: false,
+        isSaving: false,
       };
-    case actions.updateTodos:
+
+    case actions.updateTodo: {
+      const updatedTodoList = state.todoList.map((todo) =>
+        todo.id === action.editedTodo.id
+          ? {
+              ...action.editedTodo,
+              createdTime: action.editedTodo.createdTime || todo.createdTime,
+            }
+          : todo
+      )
+
+      const updatedState = {
+        ...state,
+        todoList: updatedTodoList,
+      };
+
+      if (action.error) {
+        updatedState.errorMessage = `${action.error.message}. Reverting changes...`;
+      }
+
+      return updatedState;
+    }
+
+    case actions.completeTodo: {
+      const updatedTodos = state.todoList.map((todo) =>
+        todo.id === action.id
+          ? {
+              ...todo,
+              isCompleted: true,
+            }
+          : todo
+      );
       return {
         ...state,
+        todoList: updatedTodos,
       };
-    case actions.completeTodo:
+    }
+
+    case actions.revertTodo:{
       return {
         ...state,
+        todoList: state.todoList.map((todo) =>
+          todo.id === action.originalTodo.id ? action.originalTodo : todo
+        ),
+        errorMessage: `${action.error.message}. Reverting changes...`,
       };
-    case actions.revertTodo:
-      return {
-        ...state,
-      };
+    }
+
     case actions.clearError:
       return {
         ...state,
+        errorMessage: ''
       };
-    default: return state
-  }}
 
-const initialState = {
-    todoList:[],
-    isLoading: false,
-    isSaving: false,
-    errorMessage:''
+    default:
+      return state;
+  }
 }
 
-export {initialState, actions, reducer}
+const initialState = {
+  todoList: [],
+  isLoading: false,
+  isSaving: false,
+  errorMessage: '',
+};
+
+export { initialState, actions, reducer };
